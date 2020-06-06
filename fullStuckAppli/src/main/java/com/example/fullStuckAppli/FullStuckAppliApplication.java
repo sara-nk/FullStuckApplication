0ea25a2352afe316ccc9e6ec1d397e518a79d596
snazik <snazik@LMA000981.corp.capgemini.com>
2020-06-06 11:40:17 +0100

    set up of back-end project

---
+++ fullStuckAppli/src/main/java/com/example/fullStuckAppli/FullStuckAppliApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullStuckAppli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStuckAppliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStuckAppliApplication.class, args);
+	}
+
+}
